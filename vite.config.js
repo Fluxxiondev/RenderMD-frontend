ebfa7285dfc61d8e1b459f846971c9d07e0bed3b
apsdeveloper461 <mehboobalam786461@gmail.com>
2025-10-11 00:21:10 +0500

    setup the shadcn ui theme for website and also setup tailwind and create ui button for testing of ui theme

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -9,7 +10,13 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     port: 3001,
     host: true,
